2b4ff0116ea2c49008477d78948d3cdd5ad11189
AVKdeveloper <kosov.programming@gmail.com>
2016-10-26 17:23:31 +0300

    Added heuristics to DSU

--- disjoint_set_union.h
+++ disjoint_set_union.h
@@ -5,11 +5,12 @@
 
 class DisjointSetUnion {
 	std::vector<int> parent_;
+	std::vector<int> size_; // the number of successors
 public:
 	DisjointSetUnion(const int& number_of_elements);
 	void MakeSet(const int& element);
 	void UnionSets(const int& element1, const int& element2);
-	int FindSet(const int& element) const;
+	int FindSet(const int& element);
 
 };
 
